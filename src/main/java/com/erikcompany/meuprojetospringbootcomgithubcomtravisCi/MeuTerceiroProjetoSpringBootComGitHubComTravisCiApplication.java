0bf2169cd2bcb22f36c74fa12a9b32de4747ab5b
erikflima <erik.alves253@gmail.com>
2018-08-12 15:26:42 -0300

    commit local.

--- src/main/java/com/erikcompany/meuprojetospringbootcomgithubcomtravisCi/MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication.java
+++ src/main/java/com/erikcompany/meuprojetospringbootcomgithubcomtravisCi/MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication.class, args);
+		SpringApplication.run( MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication.class, args );
 	}
 }
