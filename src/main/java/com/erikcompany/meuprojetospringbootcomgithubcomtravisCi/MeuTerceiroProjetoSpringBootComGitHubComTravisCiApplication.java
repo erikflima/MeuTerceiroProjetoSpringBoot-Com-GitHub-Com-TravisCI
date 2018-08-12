4713685d2f5f0633d64e362bacf548a19887a0d7
erikflima <erik.alves253@gmail.com>
2018-08-12 15:22:21 -0300

    Primeiro commit local

---
+++ src/main/java/com/erikcompany/meuprojetospringbootcomgithubcomtravisCi/MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication.java
@@ -0,0 +1,12 @@
+package com.erikcompany.meuprojetospringbootcomgithubcomtravisCi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/erikcompany/meuprojetospringbootcomgithubcomtravisCi/MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.erikcompany.meuprojetospringbootcomgithubcomtravisCi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeuTerceiroProjetoSpringBootComGitHubComTravisCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
